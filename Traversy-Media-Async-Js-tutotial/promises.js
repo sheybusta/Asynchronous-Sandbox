3e8f19f26e7c34c2c0ab93755c228a557ceb9052
Sheyla <shel.busta@gmail.com>
2021-07-27 20:04:36 +0200

    last update

--- Traversy-Media-Async-Js-tutotial/promises.js
+++ Traversy-Media-Async-Js-tutotial/promises.js
@@ -29,22 +29,30 @@ function createPost(post) {
   });
 }
 
-// This is to create a lot of .then otherwise you have to create Promise.all
-// createPost({ title: "Post three", body: "this is a post three" })
-//   .then(getPosts)
-//   .catch((err) => console.log(err));
+// Async / Await => function labeled async
+// elegant way to handle promises
+async function init() {
+  await createPost({ title: "post three", body: "this is post three" });
+  getPosts();
+}
+init();
 
-// Promise.all
-const promise1 = Promise.resolve("Hello World");
-const promise2 = 10;
-const promise3 = new Promise(
-  (resolve, reject) => setTimeout(resolve, 2000, "Goodbye") // we add setTimeout of 2000 millsec that is why takes 2sec to show up on console.
-);
+// // This is to create a lot of .then otherwise you have to create Promise.all
+// // createPost({ title: "Post three", body: "this is a post three" })
+// //   .then(getPosts)
+// //   .catch((err) => console.log(err));
 
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
-  (res) => res.json()
-);
-// array of promises
-Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-  console.log(values)
-);
+// // Promise.all
+// const promise1 = Promise.resolve("Hello World");
+// const promise2 = 10;
+// const promise3 = new Promise(
+//   (resolve, reject) => setTimeout(resolve, 2000, "Goodbye") // we add setTimeout of 2000 millsec that is why takes 2sec to show up on console.
+// );
+
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+//   (res) => res.json()
+// );
+// // array of promises
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//   console.log(values)
+// );
